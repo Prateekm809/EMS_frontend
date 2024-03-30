e50c78f0acabff634b0ca3865cf6ae8125effd85
prateekm809 <pm6837759@gmail.com>
2024-03-30 12:15:31 +0530

    added loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,18 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { LoaderService } from './loader.service';
-=======
->>>>>>> 7021045d6852f3286ec79659cb237c6703133c45
 
 @Component({
   selector: 'app-root',
   template: `
-<<<<<<< HEAD
     <div [ngClass]="{ 'hidden': loaderService.loaderState | async }">
       <div class="container-md">
         <router-outlet></router-outlet>
       </div>
     </div>
     <app-loader *ngIf="loaderService.loaderState | async"></app-loader>
-=======
-  <div class="container-md">
-    <router-outlet></router-outlet>
-  </div>
->>>>>>> 7021045d6852f3286ec79659cb237c6703133c45
   `,
   styles: []
 })
 export class AppComponent {
-<<<<<<< HEAD
   constructor(public loaderService: LoaderService) {}
-=======
-  title = 'client';
->>>>>>> 7021045d6852f3286ec79659cb237c6703133c45
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'; // Import HTTP_INTERCEPTORS
-=======
-import { HttpClientModule } from '@angular/common/http';
->>>>>>> 7021045d6852f3286ec79659cb237c6703133c45
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesListComponent } from './employees-list/employees-list.component';
@@ -12,12 +8,9 @@ import { EmployeeFormComponent } from './employee-form/employee-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
 import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
-<<<<<<< HEAD
 import { LoaderComponent } from './loader/loader.component';
 import { LoaderService } from './loader.service';
 import { HttpLoaderInterceptor } from './http-loader.interceptor'; // Import HttpLoaderInterceptor
-=======
->>>>>>> 7021045d6852f3286ec79659cb237c6703133c45
 
 @NgModule({
   declarations: [
@@ -25,12 +18,8 @@ import { HttpLoaderInterceptor } from './http-loader.interceptor'; // Import Htt
     EmployeesListComponent,
     EmployeeFormComponent,
     AddEmployeeComponent,
-<<<<<<< HEAD
     EditEmployeeComponent,
     LoaderComponent
-=======
-    EditEmployeeComponent
->>>>>>> 7021045d6852f3286ec79659cb237c6703133c45
   ],
   imports: [
     BrowserModule,
@@ -38,15 +27,11 @@ import { HttpLoaderInterceptor } from './http-loader.interceptor'; // Import Htt
     HttpClientModule,
     ReactiveFormsModule
   ],
-<<<<<<< HEAD
   providers: [
     LoaderService,
     // Add HttpLoaderInterceptor to the providers array
     { provide: HTTP_INTERCEPTORS, useClass: HttpLoaderInterceptor, multi: true }
   ],
-=======
-  providers: [],
->>>>>>> 7021045d6852f3286ec79659cb237c6703133c45
   bootstrap: [AppComponent]
 })
 export class AppModule { }
